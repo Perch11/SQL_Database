6e01edcd1fe2f2e9286734844b50331e26629af6
Perch <perj2000jan@gmail.com>
2023-04-04 11:56:45 +0400

    Employee Database

--- sql/SQL.sql
+++ sql/SQL.sql
@@ -1,15 +1,15 @@
 create table Employee (
-                          Employee_ID INT,
-                          Name VARCHAR(50),
-                          Gender VARCHAR(50),
-                          Position VARCHAR(50),
-                          Department VARCHAR(50),
-                          Office VARCHAR(50),
-                          Age INT,
-                          Start_Date DATE,
-                          Salary VARCHAR(50)
+                          Employee_ID INT PRIMARY KEY NOT NULL ,
+                          Name VARCHAR(50) NOT NULL ,
+                          Gender VARCHAR(50) NOT NULL ,
+                          Position VARCHAR(50) ,
+                          Department VARCHAR(50) ,
+                          Office VARCHAR(50) ,
+                          Age INT NOT NULL ,
+                          Start_Date DATE NOT NULL ,
+                          Salary VARCHAR(50) NOT NULL
 );
 
 
 insert into Employee (Employee_ID, Name, Gender, Position, Department, Office, Age, Start_Date, Salary)
-values (1, 'Cherri', 'Female', 'Librarian', 'Support', 'Twinder', 23, '7/9/2022', '$1628.60');
+values (2, 'Cherri', 'Female', 'Librarian', 'Support', 'Twinder', 23, '7/9/2022', '$1628.60');
