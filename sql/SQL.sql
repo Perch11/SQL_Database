d0fea747d66437b5e0cae2d1275168d669873386
Perch <perj2000jan@gmail.com>
2023-04-04 11:26:47 +0400

    Employee Database

---
+++ sql/SQL.sql
@@ -0,0 +1,15 @@
+create table Employee (
+                          Employee_ID INT,
+                          Name VARCHAR(50),
+                          Gender VARCHAR(50),
+                          Position VARCHAR(50),
+                          Department VARCHAR(50),
+                          Office VARCHAR(50),
+                          Age INT,
+                          Start_Date DATE,
+                          Salary VARCHAR(50)
+);
+
+
+insert into Employee (Employee_ID, Name, Gender, Position, Department, Office, Age, Start_Date, Salary)
+values (1, 'Cherri', 'Female', 'Librarian', 'Support', 'Twinder', 23, '7/9/2022', '$1628.60');
